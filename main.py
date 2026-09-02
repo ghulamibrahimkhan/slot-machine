18192e8ac1c1038cc0707c891e8bdf1f8d0154c0
Ghulam Ibrahim Khan <gulamibrahimkhan@gmail.com>
2026-09-02 15:00:01 +0500

    create get number of lines function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $: ")
@@ -11,5 +13,22 @@ def deposit():
             print("PLease enter a number.")
     return amount
 
+def get_number_of_lines():
+    while True:
+        lines = input(f"Enter a number of lines to bet on. (1-{MAX_LINES}) ? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if lines >= 1 and lines <= MAX_LINES:
+                 break
+            else:
+                 print("Enter valid numbers of line")
+        else:
+             print("PLease enter a number.")
+    return lines
+
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
