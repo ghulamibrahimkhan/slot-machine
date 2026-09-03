144034dc46e43151a8cfb54adc3585203b95121a
Ghulam Ibrahim Khan <gulamibrahimkhan@gmail.com>
2026-09-03 14:03:49 +0500

    create slot machine spin function

--- main.py
+++ main.py
@@ -7,6 +7,44 @@ MAX_BET = 100
 ROWS = 3
 COLS = 3
 
+symbols = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+symbol_value = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_sloth_machine_spin(rows,cols,symbols):
+    all_symbols = []
+    for symbol,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        colum = []
+        current_symbol = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbol)
+            current_symbol.remove(value)
+            colum.append(value)
+        columns.append(colum)
+    return columns
+
+def print_slot_columns(columns):
+    for row in range(len(columns[0])):
+        for i,column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end="|")
+            else:
+                print(column[row])
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $: ")
@@ -55,8 +93,10 @@ def main():
         bet =  get_bet()
         total_bet = bet * lines
         if total_bet > balance:
-            print(f"YOu dont have enough to bet. Your current balance is: {balance}$")
+            print(f"You dont have enough to bet. Your current balance is: {balance}$")
         else:
             break
+    slots = get_sloth_machine_spin(ROWS,COLS,symbols)
+    print_slot_columns(slots)
 
 main()
