8b29e6e261a740053c1ab9f69f182709744b0e8f
Ghulam Ibrahim Khan <gulamibrahimkhan@gmail.com>
2026-09-03 17:20:51 +0500

    final game function

--- main.py
+++ main.py
@@ -100,9 +100,7 @@ def get_bet():
     return amount
     
 
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet =  get_bet()
@@ -116,5 +114,16 @@ def main():
     winnings, winning_lines = check_winnings(slots,lines,bet,symbol_value)
     print(f"You won ${winnings}")
     print(f"You won on lines: ", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current Balance is: ${balance}")
+        answer = input("Press enter to play. (q to quit): ")
+        if answer == "q":
+            break
+        balance += spin(balance)
+    print(f"You left with ${balance}.")
 
 main()
