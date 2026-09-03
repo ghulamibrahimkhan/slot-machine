ec4364f87c02db021f942d33c589c8d4530e3d8c
Ghulam Ibrahim Khan <gulamibrahimkhan@gmail.com>
2026-09-03 15:31:07 +0500

    create check winnings function

--- main.py
+++ main.py
@@ -20,6 +20,21 @@ symbol_value = {
     "D": 8
 }
 
+def check_winnings(columns,lines,bet,values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line+1) 
+    return winnings, winning_lines
+
+
 def get_sloth_machine_spin(rows,cols,symbols):
     all_symbols = []
     for symbol,symbol_count in symbols.items():
@@ -98,5 +113,8 @@ def main():
             break
     slots = get_sloth_machine_spin(ROWS,COLS,symbols)
     print_slot_columns(slots)
+    winnings, winning_lines = check_winnings(slots,lines,bet,symbol_value)
+    print(f"You won ${winnings}")
+    print(f"You won on lines: ", *winning_lines)
 
 main()
